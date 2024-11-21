d2637f88fe9a72db233c8aae2e2fbdd5b550d69f
maguibing <flymaguibing@gmail.com>
2024-11-21 16:12:31 +0800

    update

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="size-100">
+    Hello World
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="main grid grid-cols-3 grid-rows-3 gap-20" />
+  <div class="main grid grid-cols-3 grid-rows-3 gap-20">
+    <HelloWorld />
+  </div>
 </template>
 
 <script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <style lang="scss" scoped></style>
